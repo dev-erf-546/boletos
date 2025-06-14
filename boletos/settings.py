30e97bfcaec6e04ac0a322f2cd23d9b5aae2e1a0
enka1504 <ramos_rafe@hotmail.com>
2025-06-14 07:25:05 -0600

    eliminando secret key

--- boletos/settings.py
+++ boletos/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-2+0!jsj^j%g-##c8j7x0sk5-oc&uo55yq8nouo!$npht4+g156'
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
