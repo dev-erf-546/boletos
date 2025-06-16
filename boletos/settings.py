1a9745d9ec20f0d1e0ec05f388777fb7974a34a3
enka1504 <ramos_rafe@hotmail.com>
2025-06-16 16:16:59 -0600

    correcion estatios

--- boletos/settings.py
+++ boletos/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/webapps/rifa/static' 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static_extra'),)
 MEDIA_URL = '/storage/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'storage')
 
